79ad4bca934e6113b9ee65ea048752d979e762e2
Mohammed Adebayo <adebayomuhammad47@gmail.com>
2022-09-25 08:34:00 +0100

    feat: add new layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,58 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {handleInitialData} from "../actions/shared";
-import Dashboard from "./Dashboard";
-import NewQuestion from "./NewQuestion";
-import LeaderBoard from "./LeaderBoard";
-import Login from "./Login";
-import NoMatch from "./NoMatch";
-import PrivateRoute from "./PrivateRoute";
-import LoadingBar from "react-redux-loading";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Nav from "./Nav";
-import View from "./View";
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import LoadingBar from 'react-redux-loading';
+import { handleInitialData } from '../actions/shared';
+import PrivateRoute from './PrivateRoute';
+import Dashboard from './Dashboard';
+import NewQuestion from './NewQuestion';
+import LeaderBoard from './LeaderBoard';
+import Login from './Login';
+import NoMatch from './NoMatch';
+import View from './View';
+import Layout from './layout';
 
 class App extends Component {
-    componentDidMount() {
-        this.props.dispatch(handleInitialData());
-    }
-//https://ui.dev/react-router-v4-protected-routes-authentication/
-    render() {
-        return (
-            <div>
-                <Router>
-                    <>
-                        <LoadingBar/>
-                        <div className=" ">
-                            <Nav/>
-                            <div>
-                                <Switch>
-                                    <Route path="/login"
-                                           component={Login}/>
-                                    <PrivateRoute
-                                        path="/"
-                                        exact
-                                        component={Dashboard}
-                                    />
-                                    <PrivateRoute
-                                        path="/question/:id"
-                                        exact
-                                        component={View}
-                                    />
-                                    <PrivateRoute
-                                        path="/add"
-                                        exact
-                                        component={NewQuestion}
-                                    />
-                                    <PrivateRoute
-                                        path="/leaderBoard"
-                                        exact
-                                        component={LeaderBoard}
-                                    />
-                                    <PrivateRoute path='' component={NoMatch}/>
-                                </Switch>
-                            </div>
-                        </div>
-                    </>
-                </Router>
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
+  //https://ui.dev/react-router-v4-protected-routes-authentication/
+  render() {
+    return (
+      <div>
+        <Router>
+          <>
+            <LoadingBar />
+            <div className=" ">
+              <div>
+                <Switch>
+                  <Route path="/login" component={Login} />
+                  <Layout>
+                    <PrivateRoute path="/" exact component={Dashboard} />
+                    <PrivateRoute path="/question/:id" exact component={View} />
+                    <PrivateRoute path="/add" exact component={NewQuestion} />
+                    <PrivateRoute
+                      path="/leaderBoard"
+                      exact
+                      component={LeaderBoard}
+                    />
+                  </Layout>
+                  <PrivateRoute path="" component={NoMatch} />
+                </Switch>
+              </div>
             </div>
-        );
-    }
+          </>
+        </Router>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(authedUser) {
-    return {
-
-        authedUser,
-    };
+  return {
+    authedUser
+  };
 }
 
 export default connect(mapStateToProps)(App);
---
+++ src/components/layout/index.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+
+const links = [
+  { to: '/', text: 'Dashboard', icon: 'bx-home' },
+  { to: '/add', text: 'New Questions', icon: 'bxs-plus-square' },
+  { to: '/LeaderBoard', text: 'LeaderBoard', icon: 'bx-user' }
+];
+
+const Layout = ({ children }) => {
+  const [expansionState, setExpansionState] = useState(
+    new Array(links?.length + 2).fill(false)
+  );
+
+  const handleExpansion = (position) => {
+    const updatedState = expansionState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setExpansionState(updatedState);
+  };
+
+  return (
+    <div className="grid">
+      <aside className="sidebar">
+        <nav className="sidebar__nav">
+          {links.map((link, idx) => (
+            <div className="d-flex align-items-center mb-3">
+              <NavLink
+                to={link.to}
+                onMouseEnter={() => handleExpansion(idx)}
+                onMouseLeave={() => handleExpansion(idx)}
+                className="nav-link d-flex justify-content-center align-items-center me-4"
+              >
+                <i className={`bx ${link?.icon}`}></i>
+              </NavLink>
+              <span
+                className={classNames(
+                  'd-flex justify-content-center align-items-center',
+                  {
+                    'nav-link-text': true,
+                    'nav-link-display': !expansionState[idx],
+                    'ml-1': !expansionState[idx]
+                  }
+                )}
+              >
+                {link.text}
+              </span>
+            </div>
+          ))}
+        </nav>
+
+        <nav className="sidebar__nav">
+          <div className="d-flex align-items-center mt-auto">
+            <NavLink
+              to={`/try`}
+              onMouseEnter={() => handleExpansion(5)}
+              onMouseLeave={() => handleExpansion(5)}
+              className="nav-link d-flex justify-content-center align-items-center me-4"
+            >
+              <i className="bx bx-log-out"></i>
+            </NavLink>
+            <span
+              className={classNames(
+                'd-flex justify-content-center align-items-center',
+                {
+                  'nav-link-text': true,
+                  'nav-link-display': !expansionState[5],
+                  'ml-1': !expansionState[5]
+                }
+              )}
+            >
+              Logout
+            </span>
+          </div>
+          <div className="profile p-3 mt-3">A</div>
+        </nav>
+      </aside>
+      <main className="border main">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
